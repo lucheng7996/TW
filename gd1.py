6a9269000f593466f53a8d552a810a079b147c73
lucheng7996 <lucheng7996@163.com>
2024-06-14 14:11:20 +0800

    Create gd1.py

---
+++ gd1.py
@@ -0,0 +1,34 @@
+import requests
+from lxml import etree
+import re
+import time
+
+# url = "http://104.233.187.235/4ghtml/live.html"
+url = "http://twip.xztvtv.site/"
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+}
+
+response = requests.get(url, headers=header, verify=True)
+response.encoding = 'utf-8'
+et = etree.HTML(response.text)
+#div[1]/div/div[2]/ul/li[1]
+channels = et.xpath('//div[1]/div/div[2]/ul/li')
+with open("D:/Python/TW_PP" + '.txt', 'w', encoding='utf-8') as f:
+    for channel in channels:
+        title = channel.xpath('./div/a/text()')[0]
+        title = title[1:]
+        #div[1]/div/div[2]/ul/li[1]/div/a/@href
+        play_url = channel.xpath('./div/a/@href')[0]
+        # print(title, url)
+        time.sleep(1)
+        play_resp = requests.get(play_url, headers=header, verify=True)
+        # print(play_resp.text)
+        p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
+        # print((title + "," + p_url + "\n"))
+        f.write(title + "," + p_url + "\n")
+        # break
+    print("Done!")
+
+
