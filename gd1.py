d0d39030c232d2f5d01063363795993440579cda
lucheng7996 <lucheng7996@163.com>
2024-06-23 11:38:48 +0800

    Update gd1.py

--- gd1.py
+++ gd1.py
@@ -15,7 +15,8 @@ response = requests.get(url, headers=header, proxies=proxy, verify=True)
 response.encoding = 'utf-8'
 et = etree.HTML(response.text)
 #div[1]/div/div[2]/ul/li[1]
-channels = et.xpath('//div[1]/div/div[2]/ul/li')
+#channels = et.xpath('//div[1]/div/div[2]/ul/li')
+channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li')
  # 生成txt文件
 txt_content = 'TW,#genre#\n'
 with open('TW_PP.txt', 'w', encoding='utf-8') as f:
@@ -27,12 +28,15 @@ with open('TW_PP.txt', 'w', encoding='utf-8') as f:
         play_url = channel.xpath('./div/a/@href')[0]
         # print(title, url)
         time.sleep(1)
-        play_resp = requests.get(play_url, headers=header, verify=True)
-        # print(play_resp.text)
-        p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
-        # print((title + "," + p_url + "\n"))
-        f.write(title + "," + p_url + "\n")
-        # break
+            try
+                play_resp = requests.get(play_url, headers=header, verify=True, timeout=3)
+                # print(play_resp.text)
+                #p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
+                p_url = re.findall('videoUrl: "(.*?)",', play_resp.text)[0]
+                # print((title + "," + p_url + "\n"))
+                f.write(title + "," + p_url + "\n")
+            except:
+                pass
     print("Done!")
 
 
