c768f7393137a37609e6b7b37ff1e2110966b76b
lucheng7996 <lucheng7996@163.com>
2024-06-27 17:26:22 +0800

    Update gd1.py

--- gd1.py
+++ gd1.py
@@ -6,7 +6,7 @@ import time
 url = "http://freegat.us.kg/"
 
 proxy = {
-    'http': '58.246.58.150:9002',
+    'http': '223.113.80.158:9091',
 }
 header = {
     "Referer": "http://twip.xztvtv.site/",
@@ -19,40 +19,40 @@ et = etree.HTML(response.text)
 # div[1]/div/div[2]/ul/li[1]
 # channels = et.xpath('//div[1]/div/div[2]/ul/li')
 # channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li')
-channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li') #新频道
+# channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li') #新频道
 channels1 = et.xpath('//div[1]/div/div[2]/ul/li') #特别推荐
 # 生成txt文件
 txt_content = 'TW,#genre#\n'
 with open('TW_PP.txt', 'w', encoding='utf-8') as f:
     f.write(txt_content)
-    for channel in channels:
-        title = channel.xpath('./div/a/text()')[0]
-        title = title[1:]
-        # div[1]/div/div[2]/ul/li[1]/div/a/@href
-        play_url = channel.xpath('./div/a/@href')[0]
-        # print(title, url)
-        time.sleep(1)
-        try:
-            play_resp = requests.get(play_url, headers=header, verify=True, timeout=3)
-            # print(play_resp.text)
-            # p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
-            p_url = re.findall('videoUrl: "(.*?)",', play_resp.text)[0]
-            # print((title + "," + p_url + "\n"))
-            f.write(title + "," + p_url + "\n")
-        except:
-            pass
-
+    # for channel in channels:
+    #     title = channel.xpath('./div/a/text()')[0]
+    #     title = title[1:]
+    #     # div[1]/div/div[2]/ul/li[1]/div/a/@href
+    #     play_url = channel.xpath('./div/a/@href')[0]
+    #     # print(title, url)
+    #     time.sleep(0.5)
+    #     try:
+    #         play_resp = requests.get(play_url, headers=header, verify=True, timeout=2)
+    #         # print(play_resp.text)
+    #         # p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
+    #         if play_resp.status_code == 200:
+    #             p_url = play_resp.url
+    #         # p_url = re.findall('videoUrl: "(.*?)",', play_resp.text)[0]
+    #         # print((title + "," + p_url + "\n"))
+    #         f.write(title + "," + p_url + "\n")
+    #     except:
+    #         pass
     for channel1 in channels1:
         title = channel1.xpath('./div/a/text()')[0]
         title = title[1:]
         # div[1]/div/div[2]/ul/li[1]/div/a/@href
         play_url = channel1.xpath('./div/a/@href')[0]
         # print(title, url)
-        time.sleep(1)
+        time.sleep(0.5)
         try:
-            play_resp = requests.get(play_url, headers=header, verify=True, timeout=3)
-            # print(play_resp.text)
-            # p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
+            play_resp = requests.get(play_url, headers=header, verify=True, timeout=2)
+
             p_url = re.findall('videoUrl: "(.*?)",', play_resp.text)[0]
             # print((title + "," + p_url + "\n"))
             f.write(title + "," + p_url + "\n")
