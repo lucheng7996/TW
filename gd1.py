2f36e7ee054495e049deb6e5aaaaa0e09e21826c
lucheng7996 <lucheng7996@163.com>
2024-06-27 16:33:46 +0800

    Update gd1.py

--- gd1.py
+++ gd1.py
@@ -19,8 +19,8 @@ et = etree.HTML(response.text)
 # div[1]/div/div[2]/ul/li[1]
 # channels = et.xpath('//div[1]/div/div[2]/ul/li')
 # channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li')
-channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li')
-channels1 = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li')
+channels = et.xpath('//div[1]/div/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/div[2]/ul/li') #新频道
+channels1 = et.xpath('//div[1]/div/div[2]/ul/li') #特别推荐
 # 生成txt文件
 txt_content = 'TW,#genre#\n'
 with open('TW_PP.txt', 'w', encoding='utf-8') as f:
@@ -42,20 +42,20 @@ with open('TW_PP.txt', 'w', encoding='utf-8') as f:
         except:
             pass
 
-    # for channel1 in channels1:
-    #     title = channel1.xpath('./div/a/text()')[0]
-    #     title = title[1:]
-    #     # div[1]/div/div[2]/ul/li[1]/div/a/@href
-    #     play_url = channel1.xpath('./div/a/@href')[0]
-    #     # print(title, url)
-    #     time.sleep(1)
-    #     try:
-    #         play_resp = requests.get(play_url, headers=header, verify=True, timeout=3)
-    #         # print(play_resp.text)
-    #         # p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
-    #         p_url = re.findall('videoUrl: "(.*?)",', play_resp.text)[0]
-    #         # print((title + "," + p_url + "\n"))
-    #         f.write(title + "," + p_url + "\n")
-    #     except:
-    #         pass
+    for channel1 in channels1:
+        title = channel1.xpath('./div/a/text()')[0]
+        title = title[1:]
+        # div[1]/div/div[2]/ul/li[1]/div/a/@href
+        play_url = channel1.xpath('./div/a/@href')[0]
+        # print(title, url)
+        time.sleep(1)
+        try:
+            play_resp = requests.get(play_url, headers=header, verify=True, timeout=3)
+            # print(play_resp.text)
+            # p_url = re.findall('var str ="(.*?)";', play_resp.text)[0]
+            p_url = re.findall('videoUrl: "(.*?)",', play_resp.text)[0]
+            # print((title + "," + p_url + "\n"))
+            f.write(title + "," + p_url + "\n")
+        except:
+            pass
 print("Done!")
